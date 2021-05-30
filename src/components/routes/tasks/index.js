2601e1cf3d7985f7bf4629f184f3070cdae95f26
Adib Saad <adib.saad@gmail.com>
2021-05-30 18:40:44 -0400

    Add auto-refresh and refresh button to tasks index

--- src/components/routes/tasks/index.js
+++ src/components/routes/tasks/index.js
@@ -6,12 +6,25 @@ import {
   useRef,
 } from "preact/hooks"
 import { route } from "preact-router"
+import { FontAwesomeIcon } from "@aduh95/preact-fontawesome"
+import { useLocalStorage } from "@rehooks/local-storage"
 
 import TitleContext from "@/contexts/title"
 import Table from "@/components/common/table/async"
 import TaskService from "@/service/task"
 import { taskTableColumns } from "@/components/common/tasks"
 import Button from "@/components/common/button"
+import Dropdown from "@/components/common/dropdown"
+
+const REFRESH_INT = {
+  0: "Off",
+  1000: "1s",
+  10000: "10s",
+  30000: "30s",
+  60000: "1m",
+  [5 * 60000]: "5m",
+  [10 * 60000]: "5m",
+}
 
 const Tasks = () => {
   const [tasks, setTasks] = useState([])
@@ -22,9 +35,26 @@ const Tasks = () => {
   const fetchIdRef = useRef(0)
   const fetchArgs = useRef({})
   let { setTitle } = useContext(TitleContext)
+
+  const [refreshInt, setRefreshInt] = useLocalStorage(
+    "mottainai:task-refresh-interval",
+    10000
+  )
+
   useEffect(() => {
     setTitle("Tasks")
   }, [setTitle])
+
+  useEffect(() => {
+    let int = +refreshInt
+    if (!int) {
+      return
+    }
+
+    const id = setInterval(fetchTasks, int)
+
+    return () => clearInterval(id)
+  }, [refreshInt])
 
   const fetchTasks = useCallback((args) => {
     const fetchId = ++fetchIdRef.current
@@ -55,7 +85,26 @@ const Tasks = () => {
     <>
       <div className="flex justify-between items-center mb-2">
         <p className="text-2xl font-bold mb-2">Tasks</p>
-        <div>
+        <div className="flex">
+          <Button onClick={() => fetchTasks()}>
+            <FontAwesomeIcon
+              icon="sync-alt"
+              className={`${loading ? "fa-spin" : ""} fa-fw`}
+            />
+          </Button>
+          <Dropdown
+            label={
+              <Button className="mr-1">
+                {REFRESH_INT[refreshInt]}
+                <FontAwesomeIcon icon="caret-down" className="ml-1" />
+              </Button>
+            }
+            options={Object.keys(REFRESH_INT).map((k) => ({
+              label: REFRESH_INT[k],
+              onClick: () => setRefreshInt(k),
+            }))}
+          />
+
           <Button onClick={() => route("/tasks/new")}>New Task</Button>
         </div>
       </div>
