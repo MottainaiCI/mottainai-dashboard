d4eac33698cfa6ceb0bcf3c67052d98cdcd6cb7b
Daniele Rondina <geaaru@sabayonlinux.org>
2021-08-27 00:41:10 +0200

    proxy: Add HTTPS support

--- proxy.js
+++ proxy.js
@@ -1,4 +1,7 @@
 const path = require("path")
+const fs = require('fs')
+const http = require('http');
+const https = require('https');
 const Koa = require("koa")
 const proxy = require("koa2-nginx")
 const sendfile = require("koa-sendfile")
@@ -11,9 +14,13 @@ const mount  = require('koa-mount')
 // config
 const apiUrl = process.env.API_URL || "http://localhost:9090"
 const port = process.env.PORT || 3000
-const insecure = !!process.env.INSECURE || false
+const host = process.env.HOST || "0.0.0.0"
+const insecure = process.env.INSECURE || false
 const staticDir = process.env.STATIC_DIR || path.join(__dirname, "build/")
 const appPrefix = process.env.APP_PREFIX || "/"
+const useSSL = !!process.env.USE_SSL || false
+const privateKeyFile = process.env.PRIVATE_KEYFILE || ""
+const certFile = process.env.CERT_FILE || ""
 
 process.on("SIGINT", () => {
   console.info("Exiting server")
@@ -57,7 +64,7 @@ app.use(
   })
 )
 
-console.log("Using prefix " + appPrefix)
+console.log("Using prefix " + appPrefix + "\n" + useSSL);
 const mRouter = new Router({
   prefix: appPrefix,
 });
@@ -72,6 +79,29 @@ app.use(mRouter.routes());
 app.use(mRouter.allowedMethods());
 
 app.use(logger())
-app.listen(port, function () {
+
+// Check if the certificates are availables
+if (useSSL == "true") {
+
+  if (!fs.statSync(privateKeyFile).isFile())
+    throw new Error(`Invalid private key file ${privateKeyFile}`)
+  if (!fs.statSync(certFile).isFile())
+    throw new Error(`Invalid certificate file ${certFile}`)
+
+  const certificates = {
+    key: fs.readFileSync(privateKeyFile),
+    cert: fs.readFileSync(certFile)
+  }
+
+  const server = https.createServer(certificates, app.callback())
+    .listen(port, host);
+
+  console.log("Listening on https port " + port)
+} else {
+
+  const server = http.createServer(app.callback())
+    .listen(port, host);
+
   console.log("Listening on port " + port)
-});
+}
+
